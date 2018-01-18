6c8af6e53b9173b256adad447c83e36dec0773cb
anonymousodyssey <jeffte.80@gmail.com>
2018-01-18 01:48:37 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,25 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector);
+}
+
+function nestedTarget() {
+    return document.getElementById('nested').querySelector('.target');
+}
+
+function increaseRankBy(n) {
+  let list = document.getElementById('app').querySelectorAll('ul.ranked-list li')
+
+  for (var i = 0; i < list.length; i++) {
+    list[i].innerHTML = parseInt(list[i].innerHTML) + n
+  }
+  return list
+}
+
+function deepestChild() {
+  var list = document.getElementById('grand-node').querySelectorAll('div')
+  var gotEm
+  for (let i = 0; i < list.length-1; i++) {
+      gotEm = list[i].querySelector('div');
+  }
+  return gotEm
+}
