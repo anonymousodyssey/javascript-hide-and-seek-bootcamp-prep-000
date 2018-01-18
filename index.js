3b7977998189c2fde87da0a94ac64a818ca4d4c5
anonymousodyssey <jeffte.80@gmail.com>
2018-01-18 01:27:04 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,23 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector);
+}
+
+function nestedTarget() {
+    return document.getElementById('nested').querySelector('.target');
+}
+
+function increaseRankBy(n) {
+  let list = document.getElementById('app').querySelectorAll('ul.ranked-list li')
+
+  for (var i = 0; i < list.length; i++) {
+    list[i].innerHTML = parseInt(list[i].innerHTML) + n
+  }
+  return list
+}
+
+function deepestChild() {
+  list = document.getElementById('grand-node').querySelectorAll('div')
+  for (let i =0; i < list.length; i++) {
+    
+  }
+}
